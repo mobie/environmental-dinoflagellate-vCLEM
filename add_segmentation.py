664c095ff835a3ef7df46e870833ae7d876137d1
Martin Schorb <martin.schorb@embl.de>
2023-06-16 10:22:16 +0200

    add mito segmentation

--- add_segmentation.py
+++ add_segmentation.py
@@ -4,14 +4,14 @@ import mobie
 def add_segmentation():
     path = '/g/emcf/common/Karel/Mocaeretal2022/figureAmiraV3/Segmentation_mobie_cell1/Mitochondrion/mitochondrion.tif'
 
-    resolution = [0.016] * 3
+    resolution = [16] * 3
     chunks = [128] * 3
     scale_factors = [[2, 2, 2]] * 6
 
     mobie.add_segmentation(path, '', root='./data', dataset_name='photosynthetic_dinoflagellate',
                            segmentation_name='Mitochondrion', menu_name="EM_segmentation",
                            resolution=resolution, scale_factors=scale_factors,
-                           chunks=chunks, unit='µm')
+                           chunks=chunks, unit='nm')
 
 
 if __name__ == '__main__':
